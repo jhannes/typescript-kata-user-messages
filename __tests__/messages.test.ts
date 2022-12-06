bbb8767861a1ab9442a334144aad9ff912a35ef2
Johannes Brodwall <johannes@brodwall.com>
2022-12-06 09:51:06 +0000

    🔴 It shows messages in english

--- __tests__/messages.test.ts
+++ __tests__/messages.test.ts
@@ -1,4 +1,5 @@
 const norwegian = {};
+const english = {};
 
 function showMessage(language: object, message: object) {
   return "En feil har inntruffet";
@@ -9,4 +10,8 @@ describe("translated messages", () => {
     expect(showMessage(norwegian, {code: "generalError"})).
       toBe("En feil har inntruffet");
   });
+
+  it("shows message in english", () => {
+    expect(showMessage(english, {code: "generalError"})).toBe("An error has occurred");
+  })
 });
