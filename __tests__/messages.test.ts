4dae1943759425ffc48ebbddb73ca409f5e70684
Johannes Brodwall <johannes@brodwall.com>
2022-12-06 09:56:17 +0000

    Introduce types

--- __tests__/messages.test.ts
+++ __tests__/messages.test.ts
@@ -1,20 +1,30 @@
-const norwegian = {};
-const english = {};
+type MessageTexts = {
+  generalError: string;
+};
 
-function showMessage(language: object, message: object) {
-  if (language == english) {
-    return "An error has occurred";
-  }
-  return "En feil har inntruffet";
+const norwegian: MessageTexts = {
+  generalError: "En feil har inntruffet",
+};
+const english: MessageTexts = {
+  generalError: "An error has occurred",
+};
+
+type Message = { code: "generalError" };
+
+function showMessage(language: MessageTexts, message: Message) {
+  return language[message.code];
 }
 
 describe("translated messages", () => {
   it("shows message in Norwegian", () => {
-    expect(showMessage(norwegian, {code: "generalError"})).
-      toBe("En feil har inntruffet");
+    expect(showMessage(norwegian, { code: "generalError" })).toBe(
+      "En feil har inntruffet"
+    );
   });
 
   it("shows message in english", () => {
-    expect(showMessage(english, {code: "generalError"})).toBe("An error has occurred");
-  })
+    expect(showMessage(english, { code: "generalError" })).toBe(
+      "An error has occurred"
+    );
+  });
 });
