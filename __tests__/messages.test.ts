fb9660a489907b83db1d1c5b9cb7d373632f2d5b
Johannes Brodwall <johannes@brodwall.com>
2022-12-06 09:50:16 +0000

    first message

--- __tests__/messages.test.ts
+++ __tests__/messages.test.ts
@@ -1,5 +1,12 @@
-describe("translations", () => {
-  it("translates", () => {
-    expect("true").toBe(false);
+const norwegian = {};
+
+function showMessage(language: object, message: object) {
+  return "En feil har inntruffet";
+}
+
+describe("translated messages", () => {
+  it("shows message in Norwegian", () => {
+    expect(showMessage(norwegian, {code: "generalError"})).
+      toBe("En feil har inntruffet");
   });
 });
